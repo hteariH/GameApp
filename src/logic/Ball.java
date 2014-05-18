2510556730cebf0b50ab592ae648e72c2c04c5d8
hteariH <Alexey@DoDidDoDoThis>
2014-05-19 02:31:32 +0300

    Now this is another game

--- src/logic/Ball.java
+++ src/logic/Ball.java
@@ -18,6 +18,10 @@ public class Ball extends Thread {
     GamePanel panel;
     private int vx = 1;
     private int vy = 1;
+    int tx =1;
+    private double k=100;
+//    Car car;
+    private double m;
 
     public Ball(int x, int y, GamePanel panel) {
         this.x = x;
@@ -28,33 +32,46 @@ public class Ball extends Thread {
     @Override
     public void run() {
         while (true) {
-            x += vx;
-            y += vy;
-            if (x > panel.getWidth() - diameter) {
-                x -= 2 * vx;
-                vx = -vx;
+            if(tx==90 || tx ==1){
+                vx=-vx;
             }
+            tx+=vx;
+
+            double ty = Math.sin(m*Math.toRadians(tx));
+//            double k=100;
+            y = (int) (k*ty+150);
+                    
+            System.out.println(tx+" "+y+" "+ty);
+//            if (x > panel.getWidth() - diameter) {
+//                x -= 2 * vx;
+//                vx = -vx;
+//            }
             //there is not about this game
-//            if (y > panel.getHeight() - diameter) {
+//            if (panel.getHeight()-diameter-10<y && y<panel.getHeight()-diameter && x>panel.car.getX() && x<(panel.car.getX()+panel.car.getLength())) {
 //                y -= 2 * vy;
 //                vy = -vy;
 //            }
-
-            if (x < 0) {
-                x -= 2 * vx;
-                vx = -vx;
-            }
-            if (y < 0) {
-                y -= 2 * vy;
-                vy = -vy;
-            }
-
+//
+//            if (x < 0) {
+//                x -= 2 * vx;
+//                vx = -vx;
+//            }
+//            if (y < 0) {
+//                y -= 2 * vy;
+//                vy = -vy;
+//            }
+//
+//            
             try {
                 Thread.sleep(10);
             } catch (InterruptedException ex) {
                 Logger.getLogger(Ball.class.getName()).log(Level.SEVERE, null, ex);
             }
         }
+    }
+
+    public void setK(double k) {
+        this.k = k;
     }
 
     public int getX() {
